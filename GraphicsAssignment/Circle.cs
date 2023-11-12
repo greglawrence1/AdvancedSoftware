d0979b861aa85a84a57481fd6594e643b48a9a4b
Greg Lawrence <greg_lawrence2017@outlook.com>
2023-11-12 19:05:35 +0000

    added a circle class which inherits from shape which has it's own property radius, and it's own fill status

---
+++ GraphicsAssignment/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsAssignment
+{
+    public class Circle : Shape
+    {
+        protected int radius;
+        protected bool fill;
+        public Circle(Color colour, int x, int y, int radius, bool fill) : base(colour, x, y)
+        {
+
+            this.radius = radius; //the only thingthat is different from shape
+            this.fill = fill;
+        }
+        public override void draw(Graphics g)
+        {
+            //if (x > 0 && x < 640 && y > 0 && y < 640 && radius > 0 && radius < 30)
+            {
+                Pen p = new Pen(Color.Black, 2);
+                SolidBrush b = new SolidBrush(colour);
+                if (fill)
+                {
+                    g.FillEllipse(b, x, y, radius * 2, radius * 2);
+                }
+                else
+                {
+                    g.DrawEllipse(p, x, y, radius * 2, radius * 2);
+                }
+            }
+        }
+
+        public override string ToString() //all classes inherit from object and ToString() is abstract in object
+        {
+            return base.ToString() + "  " + this.radius;
+        }
+    }
+}
