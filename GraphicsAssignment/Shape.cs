702aa4a538fccb323a79f65b88e32fab310c3c54
Greg Lawrence <greg_lawrence2017@outlook.com>
2023-11-12 19:03:54 +0000

    added an abstract shapes class which my other shapes can inherit from

---
+++ GraphicsAssignment/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsAssignment
+{
+    public abstract class Shape
+    {
+        protected Color colour;
+        protected int x;
+        protected int y;
+        public Shape(Color colour, int x, int y)
+        {
+
+            this.colour = colour; //shape's colour
+            this.x = x; //its x pos
+            this.y = y; //its y pos
+            //can't provide anything else as "shape" is too general
+        }
+
+        public abstract void draw(Graphics g); //any derrived class must implement this method
+
+        public override string ToString()
+        {
+            return base.ToString() + "    " + this.x + "," + this.y + " : ";
+        }
+    }
+}
