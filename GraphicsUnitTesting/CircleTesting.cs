89e2c721c415a1c168dc2663671d28bb53f5b112
Greg Lawrence <greg_lawrence2017@outlook.com>
2023-11-12 19:15:42 +0000

    added a small unit test for the circle to check whether the circle sets it's properties correctly

---
+++ GraphicsUnitTesting/CircleTesting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphicsAssignment;
+
+namespace GraphicsUnitTesting
+{
+    [TestClass]
+    public class circleTesting
+    {
+        [TestMethod]
+        public void CircleConstructorSetsPropertiesCorrectly()
+        {
+            // Arrange
+            Color color = Color.Red;
+            int x = 10;
+            int y = 20;
+            int radius = 5;
+            // Act
+            Circle circle = new Circle(color, x, y, radius, true);
+
+            // Assert
+            Assert.AreEqual(color, Color.Red);
+            Assert.AreEqual(x, 10);
+            Assert.AreEqual(y, 20);
+            Assert.AreEqual(radius, 5);
+        }
+
+    }
+}
