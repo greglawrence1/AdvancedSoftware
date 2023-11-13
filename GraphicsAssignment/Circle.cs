f4a38af9c52e7e368053395711069904040463e2
Greg Lawrence <greg_lawrence2017@outlook.com>
2023-11-13 00:12:23 +0000

    added the XML of the circle class

--- GraphicsAssignment/Circle.cs
+++ GraphicsAssignment/Circle.cs
@@ -7,16 +7,33 @@ using System.Threading.Tasks;
 
 namespace GraphicsAssignment
 {
+    /// <summary>
+    /// Represents a Circle that can be drawn on a windows form which inherits 
+    /// from the Shape class
+    /// </summary>
     public class Circle : Shape
     {
         protected int radius;
         protected bool fill;
+
+        /// <summary>
+        /// Initizalises the Rectangle Class
+        /// </summary>
+        /// <param name="colour">this is the colour of the circle</param>
+        /// <param name="x">this is the first coordinate of the circle</param>
+        /// <param name="y">this is the second coordinate of the circle</param>
+        /// <param name="radius">this is the width of the circle</param>
+        /// <param name="fill">this dictates whether the circle should be filled or not, if true then filled</param>
         public Circle(Color colour, int x, int y, int radius, bool fill) : base(colour, x, y)
         {
 
             this.radius = radius; //the only thingthat is different from shape
             this.fill = fill;
         }
+        /// <summary>
+        /// This does the drawing of the circle onto the graphic
+        /// </summary>
+        /// <param name="g">this is the graphic</param>
         public override void draw(Graphics g)
         {
             
