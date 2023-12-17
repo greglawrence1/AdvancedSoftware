f52ae8669b9b076e3f3f25470f0727c109c46fcc
Greg Lawrence <greg_lawrence2017@outlook.com>
2023-12-17 21:39:09 +0000

    added a fill unit test

---
+++ GraphicsUnitTesting/FillUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphicsAssignment;
+
+namespace GraphicsUnitTesting
+{
+    [TestClass]
+    public class FillUnitTest
+    {
+        [TestMethod]
+        public void FillUnitTestPractise()
+        {
+            // Arrange
+            Bitmap bitmap = new Bitmap(640, 480);
+            Graphics bitmapG = Graphics.FromImage(bitmap);
+            Parser p = new Parser(bitmapG);
+            // Act
+            p.Filled = true;
+            bool expected = true;
+           
+            // Assert
+            Assert.AreEqual(expected, p.Filled);
+        }
+    }
+}
