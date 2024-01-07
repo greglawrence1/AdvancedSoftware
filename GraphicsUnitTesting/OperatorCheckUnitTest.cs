9940654fd8dd8a8428adb482efb8601f8778549e
Greg Lawrence <greg_lawrence2017@outlook.com>
2024-01-07 18:43:36 +0000

    added two unit tests for the operatorCheck

---
+++ GraphicsUnitTesting/OperatorCheckUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GraphicsAssignment;
+
+namespace GraphicsUnitTesting
+{
+    [TestClass]
+    public class OperatorCheckUnitTest
+    {
+        [TestMethod]
+        public void TestOperatorGreaterThan()
+        {
+            Parser parser = new Parser(Graphics.FromImage(new Bitmap(1,1)));
+
+            parser.variables.Add("OperatorTest", 1);
+
+            string[] command = {"if", "OperatorTest", ">", "0" };    
+
+            bool result = parser.operatorCheck(command, 0, 1);
+            
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestOperatorGreaterThanFalse()
+        {
+            Parser parser = new Parser(Graphics.FromImage(new Bitmap(1, 1)));
+
+            parser.variables.Add("OperatorTest", 1);
+
+            string[] command = { "if", "OperatorTest", ">", "2" };
+
+            bool result = parser.operatorCheck(command, 0, 1);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
